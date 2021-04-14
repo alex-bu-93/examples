e519dcfdf46201f172ccc2535b7da0aa6f61bfb6
Alex <Bu>
2021-04-14 15:54:18 +0300

    Add - perfect scroll bar

--- src/app/views/algo-instrument/algo-instrument.module.ts
+++ src/app/views/algo-instrument/algo-instrument.module.ts
@@ -1,11 +1,12 @@
 import { NgModule }                from '@angular/core';
 import { CommonModule }            from '@angular/common';
+import { PerfectScrollbarModule }  from 'ngx-perfect-scrollbar';
 import { NzLayoutModule }          from 'ng-zorro-antd/layout';
 import { NzIconModule }            from 'ng-zorro-antd/icon';
-import { InfoCardModule }          from './widgets/info-card';
-import { AlgoInstrumentComponent } from './algo-instrument.component';
 import { NzProgressModule }        from 'ng-zorro-antd/progress';
 import { NzDividerModule }         from 'ng-zorro-antd/divider';
+import { InfoCardModule }          from './widgets/info-card';
+import { AlgoInstrumentComponent } from './algo-instrument.component';
 
 const ANT_DESIGN_MODULES = [
   NzLayoutModule,
@@ -18,6 +19,7 @@ const ANT_DESIGN_MODULES = [
   imports: [
     CommonModule,
     InfoCardModule,
+    PerfectScrollbarModule,
     ANT_DESIGN_MODULES
   ],
   declarations: [AlgoInstrumentComponent]
